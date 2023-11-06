374b446cbad6b2e9e62f6cda0d43d4f50715b0f6
Nithishkumar Selvakumar <nithishkumarselvakumar@AGLS-MLT-641.local>
2023-11-06 11:11:12 +0530

    done redux initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  increment,
+  decrement,
+  incrementByValue,
+} from "./redux/slice/counterSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
-  return <div>App</div>;
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.counter);
+
+  console.log("data", data);
+
+  return (
+    <div>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <input
+        value={data?.value}
+        type="number"
+        placeholder="enter some number"
+        onChange={(e) => dispatch(incrementByValue(Number(e.target.value)))}
+      />
+    </div>
+  );
 };
 
 export default App;
