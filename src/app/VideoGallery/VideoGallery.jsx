8766517eb6f565edb413a1d1cc165d75fd328278
Nithishkumar Selvakumar <nithishkumarselvakumar@AGLS-MLT-641.local>
2023-11-07 15:01:54 +0530

    video gallery

--- src/app/VideoGallery/VideoGallery.jsx
+++ src/app/VideoGallery/VideoGallery.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import mediaData from "../staic/data/videoGallery/mediaData.json";
+import VideoCard from "./components/VideoCard";
 
 const VideoGallery = () => {
-  return <div>VideoGallery</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(mediaData.categories);
+  }, []);
+  return (
+    <>
+      <div className="grid grid-cols-3  gap-6 p-6">
+        {data.map((item, ind) => {
+          return (
+            <div key={ind}>
+              <VideoCard item={item} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default VideoGallery;
